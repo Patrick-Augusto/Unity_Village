f98ec5a44f1b1778495bade4a6baa73e12e36180
Patrick Augusto <79328506+Patrick-Augusto@users.noreply.github.com>
2023-09-10 19:47:35 -0300

    Update Nuvens.cs

--- My project/Assets/Scripts/Nuvens.cs
+++ My project/Assets/Scripts/Nuvens.cs
@@ -5,7 +5,7 @@ public class Nuvens : MonoBehaviour
 {
     public Vector3 posicaoInicial;
     public Vector3 posicaoFinal;
-    public float velocidade = 3000.0f; // A velocidade de movimento do objeto.
+    public float velocidade = 3000.0f; 
 
     void Start()
     {
@@ -16,13 +16,13 @@ public class Nuvens : MonoBehaviour
 
     void Update()
     {
-        // Move o objeto em direção à posição final
+       
         transform.position = Vector3.MoveTowards(transform.position, posicaoFinal, velocidade * Time.deltaTime);
 
-        // Verifica se o objeto alcançou a posição final
+       
         if (transform.position == posicaoFinal)
         {
-            // Se sim, redefine a posição para a posição inicial
+           
             transform.position = posicaoInicial;
         }
     }
